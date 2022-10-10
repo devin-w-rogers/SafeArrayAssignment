e99de48b02fc78f8d3e46cf7548aaca5cf97d2d4
Devin <devin.w.rogers@gmail.com>
2022-10-10 15:25:24 -0500

    finish templating for SafeArray class

--- SafeArrayInt.h
+++ SafeArrayInt.h
@@ -1,20 +1,114 @@
 #include <cmath>
 #include <iostream>
 
+using namespace std;
+
+template <class T>
 class SafeArrayInt
 {
 public:
     SafeArrayInt();
     //~SafeArrayInt();
     int size();
-    void push_back(int newVal);
-    int pop_back();
-    int& at(int pos);
-    int& operator[](int pos);
+    void push_back(T newVal);
+    T pop_back();
+    T& at(int pos);
+    T& operator[](int pos);
 
 private:
     int capacity;
-    int* p_nums;
+    T* p_nums;
     int numElementsUsed;
 
 };
+
+template <class T>
+SafeArrayInt<T>::SafeArrayInt()
+{
+    capacity = 10;
+    p_nums = new T[capacity];
+    numElementsUsed = 0;
+}
+
+//SafeArrayInt::~SafeArrayInt()
+//{
+//    cout << "This is in the destructor" << endl;
+//}
+
+template <class T>
+int SafeArrayInt<T>::size()
+{
+    return numElementsUsed;
+}
+
+template <class T>
+void SafeArrayInt<T>::push_back(T newVal)
+{
+    if (numElementsUsed == capacity) {
+        capacity = capacity * 2;
+        T* newArray = new T[capacity];
+
+        for (int i = 0; i < capacity / 2; i++) {
+            newArray[i] = p_nums[i];
+        }
+
+        delete[] p_nums;
+        p_nums = new T[capacity];
+
+        for (int i = 0; i < capacity; i++) {
+            p_nums[i] = newArray[i];
+        }
+        delete[] newArray;
+        newArray = 0;
+    }
+    cout << "Before push_back... newVal = " << newVal << endl;
+    p_nums[numElementsUsed] = newVal;
+    numElementsUsed++;
+    cout << "After push_back..." << endl;
+}
+
+template <class T>
+T SafeArrayInt<T>::pop_back()
+{
+    T lastNum = p_nums[numElementsUsed - 1];
+    numElementsUsed--;
+
+    if (capacity != 10) {
+        if (numElementsUsed == capacity / 4) {
+            capacity = capacity / 4;
+            T* newArray = new T[capacity];
+
+            for (int i = 0; i < capacity; i++) {
+                newArray[i] = p_nums[i];
+            }
+            delete[] p_nums;
+            p_nums = new T[capacity];
+            for (int i = 0; i < capacity; i++) {
+                p_nums[i] = newArray[i];
+            }
+            delete[] newArray;
+            newArray = 0;
+        }
+    }
+
+    return lastNum;
+}
+
+template <class T>
+T& SafeArrayInt<T>::at(int pos)
+{
+    if (pos < 0 || pos >= numElementsUsed) {
+        cout << "ERROR: Tried to access an element outside the bounds..." << endl;
+        system("pause");
+        exit(0);
+    }
+    else {
+        return p_nums[pos];
+    }
+}
+
+template <class T>
+T& SafeArrayInt<T>::operator[](int pos)
+{
+    return at(pos);
+}
--- main.cpp
+++ main.cpp
@@ -6,17 +6,16 @@ using namespace std;
 
 int main()
 {
-
     //create a safe array
-    SafeArrayInt s1;
+    SafeArrayInt <double> s1;
     //verify that the number of used elements is 0
     assert(s1.size() == 0);
     cout << "s1.size = " << s1.size() << endl;
 
     //add 3 items
-    s1.push_back(10);
-    s1.push_back(20);
-    s1.push_back(30);
+    s1.push_back(10.3);
+    s1.push_back(20.8);
+    s1.push_back(30.2);
 
     //number of used elements is 3
     cout << "s1.size = " << s1.size() << endl;
@@ -27,24 +26,24 @@ int main()
     }*/
 
     //retrieve the values by position
-    assert(s1.at(0) == 10);
-    assert(s1.at(1) == 20);
-    assert(s1.at(2) == 30);
+    assert(s1.at(0) == 10.3);
+    assert(s1.at(1) == 20.8);
+    assert(s1.at(2) == 30.2);
 
     //verify the overloaded operator []    
-    assert(s1[0] == 10);
-    assert(s1[1] == 20);
-    assert(s1[2] == 30);
+    assert(s1[0] == 10.3);
+    assert(s1[1] == 20.8);
+    assert(s1[2] == 30.2);
 
     //update the first two with at()
-    s1.at(0) = 50;
-    s1.at(1) = 40;
+    s1.at(0) = 50.9;
+    s1.at(1) = 40.7;
 
     //verify
     assert(s1.size() == 3);
-    assert(s1.at(0) == 50);
-    assert(s1.at(1) == 40);
-    assert(s1.at(2) == 30);
+    assert(s1.at(0) == 50.9);
+    assert(s1.at(1) == 40.7);
+    assert(s1.at(2) == 30.2);
 
     //adding 100 random numbers
     for (int i = 0; i < 100; i++)
@@ -55,9 +54,9 @@ int main()
 
     //verify
     assert(s1.size() == 103);
-    assert(s1.at(0) == 50);
-    assert(s1.at(1) == 40);
-    assert(s1.at(2) == 30);
+    assert(s1.at(0) == 50.9);
+    assert(s1.at(1) == 40.7);
+    assert(s1.at(2) == 30.2);
 
     //removing the last 100 random numbers
     for (int i = 0; i < 100; i++)
@@ -73,14 +72,14 @@ int main()
 
     //verify
     assert(s1.size() == 3);
-    assert(s1.at(0) == 50);
-    assert(s1.at(1) == 40);
-    assert(s1.at(2) == 30);
+    assert(s1.at(0) == 50.9);
+    assert(s1.at(1) == 40.7);
+    assert(s1.at(2) == 30.2);
 
     cout << "I am at end" << endl;
 
     //going outside the bounds, the program should end
-    s1.at(3) = 40;
+    s1.at(3) = 40.7;
     cout << "You should not see this on the screen!" << endl;
 
     return 0;
